0c25dc09c3161b4ae858847e3ff45c5505912a9f
HAlGhanim <imzaji98@gmail.com>
2023-07-03 19:53:00 +0300

    payload stuff

--- utils/auth/generateToken.js
+++ utils/auth/generateToken.js
@@ -5,6 +5,8 @@ module.exports = (user) => {
   const payload = {
     _id: user._id,
     username: user.username,
+    email: user.email,
+    staff: user.staff,
   };
   const token = jwt.sign(payload, config.JWT_SECRET, {
     expiresIn: config.JWT_TOKEN_EXP,
